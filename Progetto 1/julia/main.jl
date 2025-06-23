b0bf54e4267bc0d66cd5f3587835d8f595faf42a
Leonardo-Minotti <128394176+Leonardo-Minotti@users.noreply.github.com>
2025-06-23 16:41:58 +0200

    Update main.jl

--- Progetto 1/julia/main.jl
+++ Progetto 1/julia/main.jl
@@ -2,17 +2,19 @@ using BenchmarkTools
 using MatrixMarket
 using SparseArrays
 using LinearAlgebra
-using Glob  # Per elencare i file con estensione .mtx
+using Glob
 
 function load_matrix_from_mtx(path::String)
     A = MatrixMarket.mmread(path)
     return sparse(A)
 end
 
-function is_symmetric_and_positive_definite(A::SparseMatrixCSC{Float64, Int})
+# funzione che verifica che la matrice sia simmetrica e positiva
+function cholesky_solve(A::SparseMatrixCSC{Float64, Int})
+    
     # Verifica se la matrice è simmetrica
-    if A ≈ A' #check simmetrica
-            if isposdef(A)
+    if A ≈ A' 
+            if isposdef(A) # Verifica che la matrice sia positiva
             
                 n = size(A, 1)             # Recupera numero di righe (quindi anche colonne) di A
                 xe = ones(n)               # Vettore soluzione esatta lungo n tutti 1
@@ -23,7 +25,7 @@ function is_symmetric_and_positive_definite(A::SparseMatrixCSC{Float64, Int})
                     x = F \ b              # Soluzione del sistema
                 end
 
-
+                #Calcolo errore relativo
                 rel_error = norm(x - xe) / norm(xe)
 
                 return result.time, result.bytes, rel_error
@@ -31,31 +33,29 @@ function is_symmetric_and_positive_definite(A::SparseMatrixCSC{Float64, Int})
                 println("La matrice è simmetrica ma non è definita positiva")
                 return 0,0,0
             end
-        
-        
     else
         println("✘ La matrice NON è simmetrica.")
         return 0,0,0
     end
 end
 
-#Inserisci qui il percorso alla matrice .mtx
-directory_path = "C:/Users/david/Desktop/Matrici"
+#Percorso della cartella delle matrici, inserito negli argomenti, quando si esegue il codice
+directory_path = ARGS[1]
 
-# Ottieni la lista di tutti i file .mtx nella directory
+# Ottiene la lista di tutti i file .mtx nella directory
 matrices_files = glob("*.mtx", directory_path)
 
 # Cicla su ogni file e processa
 for file_path in matrices_files
-    if file_path == "C:/Users/david/Desktop/Matrici\\G3_circuit.mtx" 
             println("Processando matrice: $file_path")
+            # carica la matrice
             A = load_matrix_from_mtx(file_path)
-            t, mem, err = is_symmetric_and_positive_definite(A)
+            # richiama la funzione per risolvere la matrice
+            t, mem, err = cholesky_solve(A)
 
-
+            #Stampa risultati
             println("Tempo: $(round(t*1000, digits=2)) ms")
             println("Memoria: $(mem ÷ 1024) KB")
             println("Errore relativo: $err")
             println("##################################")
-    end
 end
