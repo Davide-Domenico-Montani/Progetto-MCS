ae26f610dfb12c274b41a8151f076e5c744bb2d5
Leonardo-Minotti <128394176+Leonardo-Minotti@users.noreply.github.com>
2025-06-23 15:23:37 +0200

    Update Cholesky.m, sistemato codice

--- Progetto 1/MATLAB/Cholesky.m
+++ Progetto 1/MATLAB/Cholesky.m
@@ -1,25 +1,29 @@
-function Cholesky()
-    directory_path = 'C:/Users/david/Desktop/Matrici';
+function Cholesky(directory_path)
     
-    % Ottieni la lista di tutti i file .mat nella directory
+    % Ottiene la lista di tutti i file .mat nella directory
     files = dir(fullfile(directory_path, '*.mat'));
 
     % Cicla su tutti i file .mat
     for k = 1:length(files)
         file_name = files(k).name;
         file_path = fullfile(directory_path, file_name);
+        
+        fprintf('Processando matrice: %s\n', file_path);
 
-        % Salta i file specifici se necessario
-        %if ~strcmp(file_name, 'Flan_1565.mat') && ~strcmp(file_name, 'StocF-1465.mat')
-            fprintf('Processando matrice: %s\n', file_path);
-            A = load_matrix_from_mat(file_path);
-            [t, mem, rel_error] = solve_system(A);
-            clear A
-            fprintf('Tempo: %.2f ms\n', t * 1000);
-            fprintf('Memoria: %.0f KB\n', mem / 1024);
-            fprintf('Errore relativo: %.2e\n', rel_error);
-            fprintf('##################################\n');
-        %end
+        % Carica la matrice nella variabile A tramite la funzione load_matrix_from_mat
+        A = load_matrix_from_mat(file_path);
+
+        % Risolve la matrice tramite la funzione solve_system
+        [t, mem, rel_error] = solve_system(A);
+
+        % Pulisce la variabile A, in modo da caricare al prossimo ciclo la potrossima matrice
+        clear A
+
+        % Stampa performace
+        fprintf('Tempo: %.2f ms\n', t * 1000);
+        fprintf('Memoria: %.0f KB\n', mem / 1024);
+        fprintf('Errore relativo: %.2e\n', rel_error);
+        fprintf('##################################\n');
     end
 end
 
@@ -27,6 +31,7 @@ function A = load_matrix_from_mat(path)
     data = load(path);
     fields = fieldnames(data);
 
+    %le matrici salvate .mat, sono salvate in un formato struct, quindi la matrice è un componente e bisogna estrarla.
     for i = 1:length(fields)
         candidate = data.(fields{i});
 
@@ -50,9 +55,11 @@ function A = load_matrix_from_mat(path)
     error('Nessuna matrice trovata nel file MAT.');
 end
 
-function [elapsed_time, mem_used, rel_error] = solve_system(A)
-    A = sparse((A + A') / 2);  % Forza simmetria e mantiene sparsa
 
+function [elapsed_time, mem_used, rel_error] = solve_system(A)
+   
+    A = sparse((A + A') / 2);  % Forza simmetria e mantiene sparsa
+    
     n = size(A, 1);
     xe = ones(n, 1);
     b = A * xe;
@@ -61,6 +68,8 @@ function [elapsed_time, mem_used, rel_error] = solve_system(A)
 
     try
      [R, p] = chol(A);  % Salva il risultato della fattorizzazione di Cholesky in R
+
+    % Se la matrice è positiva
     if p == 0
         x = R \ (R' \ b);  % Risolve con la fattorizzazione di Cholesky
         elapsed_time = toc;
@@ -70,13 +79,14 @@ function [elapsed_time, mem_used, rel_error] = solve_system(A)
         mem_used = sum([vars_after.bytes]) - sum([vars_before.bytes]);
         clear A
     else
+        % Se la matrice non è positiva
         disp('❌ La matrice non è definita positiva.');
         elapsed_time = 0;
         mem_used = 0;
         rel_error = 0;
         clear A
     end
-    catch ME
+    catch ME %in caso di errore riposta il messaggio.
         fprintf('❌ Errore: %s\n', ME.message);
         elapsed_time = 0;
         mem_used = 0;
